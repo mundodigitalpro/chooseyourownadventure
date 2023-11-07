3bd1c761eaf37d8a02ba820d9fb90149a94cc21f
Jose Jordan <mundodigital.pro@gmail.com>
2023-11-07 12:42:40 +0100

    Load an Image

--- app/src/main/java/com/josejordan/chooseyourownadventure/MainActivity.kt
+++ app/src/main/java/com/josejordan/chooseyourownadventure/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -27,11 +28,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var choice1Button: Button
     private lateinit var choice2Button: Button
     private lateinit var storyManager: StoryManager
+    private lateinit var mainImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainImage = findViewById(R.id.mainImage)
         storyText = findViewById(R.id.storyText)
         choice1Button = findViewById(R.id.choice1Button)
         choice2Button = findViewById(R.id.choice2Button)
